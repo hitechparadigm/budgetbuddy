b331c5da6defcdec2a2d13e220eb280b3d5c7e5e
Dmytro Malyk <dmytro.malyk@gmail.com>
2025-10-24 17:46:23 -0400

    fix: simplify ESLint configuration to focus on infrastructure

--- infrastructure/.eslintrc.js
+++ infrastructure/.eslintrc.js
@@ -7,41 +7,24 @@ module.exports = {
     extends: [
         'eslint:recommended',
         '@typescript-eslint/recommended',
-        'prettier',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2022,
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint', 'prettier'],
+    plugins: ['@typescript-eslint'],
     rules: {
-        'prettier/prettier': 'error',
         '@typescript-eslint/no-unused-vars': ['error', {
             argsIgnorePattern: '^_'
         }],
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'warn',
-        'no-console': 'warn',
+        'no-console': 'off', // Allow console.log in CDK code
     },
-    overrides: [{
-            files: ['**/*.test.ts', '**/*.test.js'],
-            env: {
-                jest: true,
-            },
-        },
-        {
-            files: ['infrastructure/**/*.ts'],
-            rules: {
-                'no-console': 'off', // Allow console.log in CDK code
-            },
-        },
-    ],
     ignorePatterns: [
         'node_modules/',
-        'dist/',
-        'build/',
         'cdk.out/',
         '*.d.ts',
     ],
